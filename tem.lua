9edab9d3563e46247af0ba651a5caed604f2b721
seven-it-work <75720932+seven-it-work@users.noreply.github.com>
2025-04-29 15:56:02 +0800

    Update tem.lua

--- tem.lua
+++ tem.lua
@@ -3,15 +3,12 @@ local data = {
     -- 表中包含一个名为 "盘龙飞舟" 的子表
     ["盘龙飞舟"] = {
         key = "55D;5D;225D::9",  -- 这里使用 = 而不是 :，并且去掉了逗号
-        offsets = {0x40, 0x80, 0xC0}
     },
     ["磐石靴"] = {
         key = "59D;17D;174D::9",  -- 这里使用 = 而不是 :，并且去掉了逗号
-        offsets = {0x80, 0x100, 0x180}
     },
     ["九霄配环"] = {
         key = "0D;17D;1,644D::9",  -- 这里使用 = 而不是 :，并且去掉了逗号
-        offsets = {0x40, 0x80, 0x100}
     },
     
 }
@@ -34,6 +31,14 @@ local function extractNumberFromKey(key)
     return nil
 end
 
+-- 检查这个值的范围
+local func checkNumber(value)
+    if value >=10 && value <= 433333 then
+        return true
+    end
+return false
+end
+
 -- 定义一个函数来处理搜索和偏移量操作
 local function processItem(itemKey)
 
@@ -51,32 +56,35 @@ local function processItem(itemKey)
             print("无法从 key 中提取精炼数字: ", itemData.key)
             return
         end
-        local t = gg.getResults(500)
+        local t = gg.getResults(10)
         if #t <= 0 then
-        gg.toast(itemKey.."修改失败")
-        print(itemKey.."修改失败")
-        return
+            gg.toast(itemKey.."修改失败")
+            print(itemKey.."修改失败")
+            return
         end
 
         -- 遍历搜索结果并处理偏移量
         for _, item in ipairs(t) do
-            item.name=itemKey
-            item.value=9999999
-            for _, offset in ipairs(itemData.offsets) do
-            --print(_)
-                local r = {}
-                r[1] = {}
-                r[1].address = item.address + offset
-                r[1].flags = 4
-                r[1].value = 9999999
-                r[1].freeze = false
-                r[1].name = itemKey.._
-                gg.setValues(r)
-                gg.addListItems(r)
+            if checkNumber(item.value) then
+                item.name=itemKey
+                item.value=9999999
+                gg.setValues(t)
+                gg.addListItems(t)
+                local offset = 0x00
+                for i = 1, 10 do
+                    offset = offset + 0x40
+                    gg.clearResults()
+                    gg.searchAddress(item.address + offset, 0xFFFFFFFF)
+                    local r = gg.getResults(1)
+                    if checkNumber(r[1].value) then
+                        r[1].value = 9999999
+                        r[1].name = itemKey.._
+                        gg.setValues(r)
+                        gg.addListItems(r)
+                    end
+                end
             end
         end
-        gg.setValues(t)
-        gg.addListItems(t)
         gg.toast(itemKey.."修改成功")
     else
         print("未找到对应的 item 数据: ", itemKey)
